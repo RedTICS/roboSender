e735ef4464f0fd841fb8dfa9b416e1af6b7d5dac
hugo <hfernandez@neuquen.gov.ar>
2017-10-05 16:05:47 -0300

    control lista vacia

--- sender.ts
+++ sender.ts
@@ -16,42 +16,46 @@ mongoClient.connect(urlAndes, function (err, db) {
         if (error) {
             console.log(error);
         }
+        if (enviosPendientes.length > 0) {
+            enviosPendientes.forEach(async env => {
+                try {
+                    console.log('enver: ', env);
+                    if (env.email) {
+                        let mailOptions: any = {
+                            from: configPrivate.enviarMail.host,
+                            to: env.email,
+                            subject: env.subject,
+                            text: env.message,
+                            html: env.message
+                        };
+                        await mailTools.sendMail(mailOptions);
+                    }
+                    if (env.phone) {
+                        let smsOptions: any = {
+                            telefono: env.phone,
+                            mensaje: env.message
+                        };
+                        await smsTools.sendSms(smsOptions)
+                    }
+                    await changeState(db, env, 'success');
 
-        enviosPendientes.forEach(async env => {
-            try {
-                if (env.email) {
-                    let mailOptions: any = {
-                        from: configPrivate.enviarMail.host,
-                        to: env.email,
-                        subject: env.subject,
-                        text: env.message,
-                        html: env.message
-                    };
-                    await mailTools.sendMail(mailOptions);
+                } catch (errorSending) {
+                    if (env.tries > 5) {
+                        await changeState(db, env, 'error');
+                    } else {
+                        await changeState(db, env, 'pending');
+                    }
                 }
-                if (env.phone) {
-                    let smsOptions: any = {
-                        telefono: env.phone,
-                        mensaje: env.message
-                    };
-                    await smsTools.sendSms(smsOptions)
+                counter++;
+                if (counter === enviosPendientes.length) {
+                    db.close();
+                    console.log('Proceso finalizado: ', Date.now())
                 }
-                await changeState(db, env, 'success');
-                
-            } catch (errorSending) {
-                if (env.tries > 5) {
-                    await changeState(db, env, 'error');
-                } else {
-                    await changeState(db, env, 'pending');
-                }
-            }
-            counter++;
-            if (counter === enviosPendientes.length) {
-                db.close();
-                console.log('Proceso finalizado: ', Date.now())
-            }
-        });
-        
+            });
+        } else {
+            db.close();
+            console.log('Proceso finalizado, nada para enviar: ', Date.now());
+        }
 
     });
 });
